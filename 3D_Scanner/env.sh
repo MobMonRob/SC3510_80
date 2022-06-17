6bf683d8a130a46ee359f55480c31341d9e94072
Saadalh <104773395+Saadalh@users.noreply.github.com>
2022-06-17 13:56:34 +0200

    Update env.sh

--- 3D_Scanner/env.sh
+++ 3D_Scanner/env.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-export LD_LIBRARY_PATH=/home/nitish/Desktop/Cpp/3D-Sensor-SDK-1.6.2/Libs #absolute path to your Lib directory
-export GENICAM_ROOT_V3_1=/home/nitish/Desktop/Cpp/3D-Sensor-SDK-1.6.2/Libs #absolute path to the Lib directory
+export LD_LIBRARY_PATH=/home/nitish/Desktop/Cpp/3D-Sensor/Libs #absolute path to your Lib directory
+export GENICAM_ROOT_V3_1=/home/nitish/Desktop/Cpp/3D-Sensor/Libs #absolute path to the Lib directory
 
